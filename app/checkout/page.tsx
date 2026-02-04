d79a9385cc9158144f7d1f5e9858ce8a9d719e62
Anastasiia Ustin <2anastasiia@mail.ru>
2026-02-04 19:09:55 +0200

    added case when the cart is not empty

--- app/checkout/page.tsx
+++ app/checkout/page.tsx
@@ -1,4 +1,5 @@
 'use client'
+import { Card, CardContent, CardHeader, CardTitle } from "@/components/ui/card"
 import { useCartStore } from "@/store/storeCart"
 
 export default function CheckoutPage() {
@@ -6,9 +7,28 @@ export default function CheckoutPage() {
     const {items} = useCartStore()
     const total = items.reduce((acc, item) => acc + (item.quantity * item.price), 0)
 
-    if (total || items.length) {
+    if (items.length === 0) {
         return <div> <h1>Your Cart is Empty.</h1> </div>
     }
 
-    return <p>checkout page</p>
+    return <div>
+        <h1>Checkout</h1>
+        <Card>
+            <CardHeader>
+                <CardTitle>Order Summary</CardTitle>
+            </CardHeader>
+            <CardContent>
+                <ul>
+                    {items.map(item => (
+                        <li key={item.name}>
+                            <div>
+                                <span>{item.name}</span>
+                                <span>${((item.price * item.quantity) / 100).toFixed(2)}</span>
+                            </div>
+                        </li>
+                    ))}
+                </ul>
+            </CardContent>
+        </Card>
+    </div>
 }

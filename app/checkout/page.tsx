9116fea26f3d2e2c3292ca7df24be2b5d08d5d90
Anastasiia Ustin <2anastasiia@mail.ru>
2026-02-04 19:16:29 +0200

    added total amount for display and removeItem with addItem functionality in Checkout

--- app/checkout/page.tsx
+++ app/checkout/page.tsx
@@ -1,10 +1,11 @@
 'use client'
+import { Button } from "@/components/ui/button"
 import { Card, CardContent, CardHeader, CardTitle } from "@/components/ui/card"
 import { useCartStore } from "@/store/storeCart"
 
 export default function CheckoutPage() {
 
-    const {items} = useCartStore()
+    const {items, removeItem, addItem} = useCartStore()
     const total = items.reduce((acc, item) => acc + (item.quantity * item.price), 0)
 
     if (items.length === 0) {
@@ -25,9 +26,17 @@ export default function CheckoutPage() {
                                 <span>{item.name}</span>
                                 <span>${((item.price * item.quantity) / 100).toFixed(2)}</span>
                             </div>
+                            <div>
+                                 <Button onClick={() => removeItem(item.id)} variant="outline">-</Button>
+                                 <span className="text-lg font-semibold">{item.quantity}</span>
+                                 <Button onClick={() => addItem({ ...item, quantity: 1 })} variant="outline">+</Button>
+                            </div>
                         </li>
                     ))}
                 </ul>
+                <div>
+                    Total: ${(total / 100).toFixed(2)}
+                </div>
             </CardContent>
         </Card>
     </div>

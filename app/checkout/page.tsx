56c2793b9fd1569e82c24c17e61229f11651f6cd
Anastasiia Ustin <2anastasiia@mail.ru>
2026-02-04 19:02:58 +0200

    added total  sum of cart and processed the case whan the cart is empty

--- app/checkout/page.tsx
+++ app/checkout/page.tsx
@@ -1,3 +1,14 @@
+'use client'
+import { useCartStore } from "@/store/storeCart"
+
 export default function CheckoutPage() {
+
+    const {items} = useCartStore()
+    const total = items.reduce((acc, item) => acc + (item.quantity * item.price), 0)
+
+    if (total || items.length) {
+        return <div> <h1>Your Cart is Empty.</h1> </div>
+    }
+
     return <p>checkout page</p>
 }

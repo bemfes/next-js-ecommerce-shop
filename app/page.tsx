8beef3e15560d86da1c0ab910986dc33409aa34f
Anastasiia Ustin <2anastasiia@mail.ru>
2026-02-04 13:32:53 +0200

    did code for fetching products from stripe acc to home page

--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,14 @@
+import { stripe } from "@/lib/stripe";
 
 
-export default function Home() {
+export default async function Home() {
+  const products = await stripe.products.list(
+    {
+      expand: ['data.default_price'],
+      limit: 5
+    }
+  )
+  console.log(products);
   return (
     <div></div>
   );

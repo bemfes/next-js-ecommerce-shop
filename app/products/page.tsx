c9fc4f38d2651a7b6225140ac8686472bc3b001a
Anastasiia Ustin <2anastasiia@mail.ru>
2026-02-04 15:54:45 +0200

    created basic markdown for Products page

--- app/products/page.tsx
+++ app/products/page.tsx
@@ -1,3 +1,13 @@
-export default function ProductsPage() {
-    return <p>products page</p>
+import { stripe } from "@/lib/stripe"
+
+export default async function ProductsPage() {
+     const products = await stripe.products.list(
+        {
+          expand: ['data.default_price']
+        }
+      )
+    return <div>
+        <h1>All Products</h1>
+        
+    </div>
 }

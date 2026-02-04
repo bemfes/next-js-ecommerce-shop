45a232a12c64c5c715c34e0e2d23fe6140262ef2
Anastasiia Ustin <2anastasiia@mail.ru>
2026-02-04 16:03:36 +0200

    added image, price and title to the ProductCard

--- components/ProductCard.tsx
+++ components/ProductCard.tsx
@@ -1,11 +1,30 @@
+
+import Link from "next/link"
 import Stripe from "stripe"
+import { Card, CardContent, CardHeader, CardTitle } from "./ui/card"
+import Image from "next/image"
 
 interface Props {
     product: Stripe.Product
 }
 
 export const ProductCard = ({product}: Props) => {
-    return <div>
-        
-    </div>
+    const price = product.default_price as Stripe.Price
+    return <Link href='/products/1'>
+        <Card>
+            {product.images && product.images[0] && (
+                <div className="relative h-80 w-full">
+                    <Image className="transition-opacity duration-500 ease-in-out" layout="fill" objectFit="cover" alt={product.name} src={product.images[0]}/>
+                </div>
+                )}
+            <CardHeader>
+                <CardTitle>{product.name}</CardTitle>
+                <CardContent>
+                {price && price.unit_amount && (
+                    <p className="text-xl text-white">${(price.unit_amount / 100).toFixed(2)}</p>
+                )}
+                </CardContent>
+            </CardHeader>
+        </Card>
+    </Link>
 }

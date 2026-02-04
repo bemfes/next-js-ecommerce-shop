b3d4d764e2f7d7f47309550bf864491de83aa59c
Anastasiia Ustin <2anastasiia@mail.ru>
2026-02-04 16:30:03 +0200

    did filtering products mechanism while searching

--- components/ProductList.tsx
+++ components/ProductList.tsx
@@ -1,20 +1,34 @@
+'use client'
 import Stripe from "stripe"
 import { ProductCard } from "./ProductCard"
+import { useState } from "react"
 
 interface Props {
     products: Stripe.Product[]
 }
 
 export const ProductList = ({products}: Props) => {
+
+    const [searchItem, setSearchItem] = useState<string>('')
+
+    const filteredProducts = products.filter((product) => {
+        const item = searchItem.toLowerCase()
+        const nameMatch = product.name.toLocaleLowerCase().includes(item)
+        const descriptionMatch = product.description ? product.description.toLocaleLowerCase().includes(item) : false
+        return nameMatch || descriptionMatch
+    })
+
     return <div>
         <div className="mb-6 flex justify-center">
             <input 
+            value={searchItem}
+            onChange={(e) => setSearchItem(e.target.value)}
             className="w-full max-w-md rounded border border-gray-300 px-4 py-2 focus:outline-none focus:ring-2 focus:ring-blue-500" 
             type="text" 
             placeholder="Search products..." />
         </div>
         <ul className="mt-6 grid grid-cols-1 gap-4 sm:grid-cols-2 lg:grid-cols-3">
-            {products.map((product) => {
+            {filteredProducts.map((product) => {
                 return <li key={product.name}>
                     <ProductCard product={product}></ProductCard>
                 </li>

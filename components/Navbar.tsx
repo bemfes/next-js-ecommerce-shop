ee40f8c5990456df5558ae71e8bbfa107579bb67
Anastasiia Ustin <2anastasiia@mail.ru>
2026-02-04 18:17:19 +0200

    added cartCount sign to the Navbar

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -1,7 +1,11 @@
+import { useCartStore } from "@/store/storeCart"
 import { ShoppingCartIcon } from "@heroicons/react/24/outline"
 import Link from "next/link"
 
 export const Navbar = () => {
+    const { items } = useCartStore()
+    const cartCount = items.reduce((acc, item) => acc + item.quantity, 0)
+
     return <nav className="sticky top-0 z-50 bg-white shadow"> 
         <div className="container mx-auto flex items-center justify-between px-4 py-4">
             <Link href="/" className="hover:text-blue-600">My Ecommerce</Link>
@@ -14,6 +18,7 @@ export const Navbar = () => {
             <div className="flex items-center space-x-4">
                 <Link href="/checkout">
                     <ShoppingCartIcon/>
+                    { cartCount > 0 && <span>{cartCount}</span> }
                 </Link>
             </div>
         </div>

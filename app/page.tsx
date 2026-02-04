7811842593dd04259066a9ac3357d52a98969474
Anastasiia Ustin <2anastasiia@mail.ru>
2026-02-04 14:04:59 +0200

    added styles to the main page

--- app/page.tsx
+++ app/page.tsx
@@ -16,19 +16,19 @@ export default async function Home() {
   console.log(products);
   return (
     <div>
-      <section>
-        <div>
-          <div>
-            <h2>Welcome to My Ecommerce</h2>
-            <p>Discover the latest products at the best prices</p>
-            <Button asChild variant="default">
-              <Link href="/products">Browse all products</Link>
+      <section className="rounded bg-neutral-100 py-8 sm:py-12">
+        <div className="mx-auto grid grid-cols-1 items-center justify-items-center gap-8 px-8 sm:px-16 md:grid-cols-2">
+          <div className="max-w-md space-y-4">
+            <h2 className="text-3xl font-bold tracking-tight md:text-4xl">Welcome to My Ecommerce</h2>
+            <p className="text-neutral-600">Discover the latest products at the best prices</p>
+            <Button className="inline-flex items-center justify-center rounded-full px-6 py-3 bg-black text-white" asChild variant="default">
+              <Link className="inline-flex items-center justify-center rounded-full px-6 py-3" href="/products">Browse all products</Link>
             </Button>
           </div>
-          <Image src={products.data[0].images[0]} alt="Banner image" width={450} height={450}/>
+          <Image src={products.data[2].images[0]} alt="Banner image" width={450} height={450}/>
         </div>
       </section>
-      <section>
+      <section className="py-8">
         <Carousel/>
       </section>
     </div>

19f27328fe61d3f80ea15d4a45af2a37bc4b8711
Anastasiia Ustin <2anastasiia@mail.ru>
2026-02-07 19:16:16 +0200

    added dynamic metadata for individual product page

--- app/products/[id]/page.tsx
+++ app/products/[id]/page.tsx
@@ -1,6 +1,22 @@
 import { ProductDetail } from "@/components/ProductDetails"
 import { stripe } from "@/lib/stripe"
+import { Metadata } from "next"
 
+export async function generateMetadata({params}: {params: { id: string }}): Promise<Metadata> {
+    const product = await stripe.products.retrieve(params.id, {
+        expand: ['default_price']
+    })
+    if (!product) {
+        return {
+            title: 'Not found',
+            description: 'The requested product could not be found'
+        }
+    }
+    return {
+        title: `${product.name} | My Ecommerce Shop`,
+        description: `${product.description}`
+    }
+}
 
 export default async function ProductDetailsPage({params}: {params: { id: string }}) {
     const product = await stripe.products.retrieve(params.id, {

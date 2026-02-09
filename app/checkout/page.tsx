2e27741c9d1b4f88382663fb22fc78bb204a0950
Anastasiia Ustin <2anastasiia@mail.ru>
2026-02-09 11:52:45 +0200

    added metadata for the Checkout page

--- app/checkout/page.tsx
+++ app/checkout/page.tsx
@@ -1,6 +1,9 @@
+import { Metadata } from "next";
 import CheckoutPageContent from "./page.client"
 
-
+export const metadata: Metadata = {
+  title: "Checkout page",
+};
 
 
 export default function CheckoutPage() {
